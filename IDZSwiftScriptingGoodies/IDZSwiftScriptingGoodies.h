ea73dfc0f6cf11db56117d1f788905cbf020b9fe
iosdevzone <idz@iosdeveloperzone.com>
2015-11-13 20:49:43 -0800

    Initial GitHub checkin.

---
+++ IDZSwiftScriptingGoodies/IDZSwiftScriptingGoodies.h
@@ -0,0 +1,19 @@
+//
+//  IDZSwiftScriptingGoodies.h
+//  IDZSwiftScriptingGoodies
+//
+//  Created by idz on 11/13/15.
+//  Copyright © 2015 iOS Developer Zone. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for IDZSwiftScriptingGoodies.
+FOUNDATION_EXPORT double IDZSwiftScriptingGoodiesVersionNumber;
+
+//! Project version string for IDZSwiftScriptingGoodies.
+FOUNDATION_EXPORT const unsigned char IDZSwiftScriptingGoodiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDZSwiftScriptingGoodies/PublicHeader.h>
+
+
---
+++ IDZSwiftScriptingGoodies/String.swift
@@ -0,0 +1,20 @@
+//
+//  String.swift
+//  IDZSwiftScriptingGoodies
+//
+//  Created by idz on 11/13/15.
+//  Copyright © 2015 iOS Developer Zone. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    /**
+        Trims trailing whitespace and newlines.
+
+        - returns: the trimmed string.
+    */
+    public func trim() -> String {
+        return ((self as NSString).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as String)
+    }
+}
