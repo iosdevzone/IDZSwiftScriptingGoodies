0d3605a6332050a8a4e8430ae6de2108dd49c131
iosdevzone <idz@iosdeveloperzone.com>
2015-11-14 13:49:25 -0800

    Added string colors

--- IDZSwiftScriptingGoodies/String.swift
+++ IDZSwiftScriptingGoodies/String.swift
@@ -18,3 +18,15 @@ public extension String {
         return ((self as NSString).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as String)
     }
 }
+
+public extension String {
+    public func CSI(code: String) -> String { return "\u{001b}[" + code }
+    public var black: String { return CSI("30m") + self + CSI("0m") }
+    public var red: String { return CSI("31m") + self + CSI("0m") }
+    public var green: String { return CSI("32m") + self + CSI("0m") }
+    public var yellow: String { return CSI("33m") + self + CSI("0m") }
+    public var blue: String { return CSI("34m") + self + CSI("0m") }
+    public var magenta: String { return CSI("35m") + self + CSI("0m") }
+    public var cyan: String { return CSI("36m") + self + CSI("0m") }
+    public var white: String { return CSI("37m") + self + CSI("0m") }
+}
